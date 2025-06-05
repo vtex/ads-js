56008501c5d6535c1ef46a495e5b76b2e6943d09
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-05 16:16:30 -0300

    fix: test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    projects: [
-      'packages/core',
-      'packages/react',
-    ],
-  },
-})
+  test: {},
+});
