5a1570361e64829ffe80249268f3718abd6579cf
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-09 14:37:51 -0300

    chore: setup monorepo with build config (#1)
    * chore: setup monorepo with build config
    
    * fix: remove unused yarn.lock
    
    * feat: add github ci
    
    * fix: test config
    
    * feat: setup test environment

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  eslintPluginPrettierRecommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          comments: 80,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+])
---
+++ packages/core/src/example.test.ts
@@ -0,0 +1,5 @@
+import { it, expect } from 'vitest';
+
+it('should run a simple test', () => {
+  expect(1 + 1).toBe(2);
+})
