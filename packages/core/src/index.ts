2b6b248180cb9e141cde43b45dd66da24b441279
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-24 09:17:36 -0300

    feat: export hydration strategy components

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,11 @@
 import { getRawAds, getHydratedAds, getISHydratedAdsUncached } from "./facade";
+import { fetchWithIS } from "./hydration/intelligentSearchFetcher/fetchWithIS";
+import { searchProductMatchesOffer } from "./hydration/intelligentSearchFetcher/searchProductMatchesOffer";
 
-export { getRawAds, getHydratedAds, getISHydratedAdsUncached };
+export {
+  getRawAds,
+  getHydratedAds,
+  getISHydratedAdsUncached,
+  fetchWithIS,
+  searchProductMatchesOffer,
+};
