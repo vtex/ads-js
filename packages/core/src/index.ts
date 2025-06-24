f4c73361ab9b4d180c5a78a17448246ad42715c6
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-24 09:20:39 -0300

    feat: export some types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,10 @@ import { getRawAds, getHydratedAds, getISHydratedAdsUncached } from "./facade";
 import { fetchWithIS } from "./hydration/intelligentSearchFetcher/fetchWithIS";
 import { searchProductMatchesOffer } from "./hydration/intelligentSearchFetcher/searchProductMatchesOffer";
 
+export * from "./types";
+export * from "./clients/search/types";
+export * from "./clients/adServer/types";
+
 export {
   getRawAds,
   getHydratedAds,
