802df2a6814b20c219b2eab7e7257ceba3a7c55e
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-25 09:54:31 -0300

    fix: make base64 isomorphic
    On server, window is not a variable with value `undefined`, it's a
    variable not defined which access throws error. So we must check it's
    existence using `typeof` operator.

--- packages/core/src/utils/base64.ts
+++ packages/core/src/utils/base64.ts
@@ -1,12 +1,12 @@
 export const atob = (val: string) => {
-  if (window.atob) {
+  if (typeof window !== "undefined" && window.atob) {
     return window.atob(val);
   }
   return Buffer.from(val, "base64").toString();
 };
 
 export const btoa = (val: string) => {
-  if (window.btoa) {
+  if (typeof window !== "undefined" && window.btoa) {
     return window.btoa(val);
   }
   return Buffer.from(val).toString("base64");
