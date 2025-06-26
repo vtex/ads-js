1eef64a3ea844f3d7ecb2bdb46b74690749b26c3
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-06-26 16:43:19 -0300

    refactor: be more explicit about ads core exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,15 +1,15 @@
-import { getRawAds, getHydratedAds, getISHydratedAdsUncached } from "./facade";
-import { fetchWithIS } from "./hydration/intelligentSearchFetcher/fetchWithIS";
-import { searchProductMatchesOffer } from "./hydration/intelligentSearchFetcher/searchProductMatchesOffer";
-
-export * from "./types";
 export * from "./clients/search/types";
 export * from "./clients/adServer/types";
 
+export { getRawAds, getHydratedAds, getISHydratedAdsUncached } from "./facade";
+export { fetchWithIS } from "./hydration/intelligentSearchFetcher/fetchWithIS";
+export { searchProductMatchesOffer } from "./hydration/intelligentSearchFetcher/searchProductMatchesOffer";
+
 export {
-  getRawAds,
-  getHydratedAds,
-  getISHydratedAdsUncached,
-  fetchWithIS,
-  searchProductMatchesOffer,
-};
+  RawAdsByPlacements,
+  RawAdsResponse,
+  Facet,
+  Identity,
+  GetAdsArgs,
+} from "./types";
+export { ProductFetcher, ProductMatchesOffer } from "./hydration/types";
