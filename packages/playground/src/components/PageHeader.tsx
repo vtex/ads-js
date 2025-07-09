d23d516f4c7ab824efd1931e77909eba811dad38
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-07-09 10:21:45 -0300

    feat: link docs in playground

--- packages/playground/src/components/PageHeader.tsx
+++ packages/playground/src/components/PageHeader.tsx
@@ -3,11 +3,12 @@ import React from "react";
 export function PageHeader() {
   return (
     <div className="mb-8">
-      <h2 className="text-3xl text-vtex-black mb-2">Ads SDK Playground</h2>
+      <h2 className="text-3xl text-vtex-black mb-2">Ads React SDK Playground</h2>
       <p className="text-vtex-gray max-w-2xl">
         Configure and test VTEX Ads platform with different hydration
         strategies, search parameters, and facets. Experience how our React SDK
-        integrates seamlessly with your commerce platform.
+        integrates seamlessly with your commerce platform. Check full <a
+        href="https://vtex.github.io/ads-js/">SDK docs</a>.
       </p>
     </div>
   );
